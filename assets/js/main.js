be7071f8d9f434ce61d44fef77662c9b06e3764c
mvshahin <vip.sahin.00@bk.ru>
2021-09-04 22:17:29 +0400

    yeni

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,24 @@
         $('body').removeClass('overflowY-hidden')
     })
 
+
+
+
+
+
+    $(document).on('click', '.hover-shadow', function () {
+        $('body').addClass('overflowY-hidden')
+        $('#myModal').addClass('overflowY-hidden')
+    })
+
+    $(document).on('click', '.close', function () {
+        $('body').removeClass('overflowY-hidden')
+    })
+
+
+
+
+
     // Preloader
     $(window).on('load', function () {
         if ($('#preloader').length) {
@@ -301,33 +319,107 @@ validation.validate();
 
 
 // Get the modal
-var modal = document.getElementById("myModal");
-const img1 = document.getElementById("imgid");
+// var modal = document.getElementById("myModal");
+// const img1 = document.getElementById("imgid");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = img1.src;
-  captionText.innerHTML = img1.alt;
-}
+// var img = document.getElementById("myImg");
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = img1.src;
+//   captionText.innerHTML = img1.alt;
+// }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
 }
-
-
-
-
-
-
-
-
-
-
+  
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+  
+var slideIndex = 1;
+showSlides(slideIndex);
+  
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+  
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+  
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+}
